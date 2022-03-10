01676e4f3816e24c29a31cea3379b69c9d8e8020
jacey2011 <101364267+jacey2011@users.noreply.github.com>
2022-03-10 15:49:57 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,189 @@
+let berriesLeft = 4 
+info.setLife(3)
+let crossingTime = 20 
+info.startCountdown(crossingTime)
+namespace SpriteKind {
+    export const UncollectedBerry = SpriteKind.create()
+    export const CollectedBerry = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    Fish.y-=16
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    Fish.x -= 16
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    Fish.x += 16
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    Fish.y += 16
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileGrass2, function (sprite, location) {
+    if (heldBerry != null) {
+        info.startCountdown(20)
+        tiles.placeOnTile(heldBerry, location)
+        heldBerry.setImage(img`
+            . . . . . . . 6 . . . . . . . . 
+            . . . . . . 8 6 6 . . . 6 8 . . 
+            . . . e e e 8 8 6 6 . 6 7 8 . . 
+            . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+            . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+            . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+            e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+            e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+            e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+            e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+            e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+            e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+            e e 2 e 2 2 4 2 2 e e e c . . . 
+            e e e e 2 e 2 2 e e e c . . . . 
+            e e e 2 e e c e c c c . . . . . 
+            . c c c c c c c . . . . . . . . 
+            `)
+        heldBerry = null
+         if (berriesLeft == 0 ){
+             game.over(true )
+         }
+//console.log("dropped berry")
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.UncollectedBerry, function (playerSprite, berrySprite) {
+    if (heldBerry == null) {
+        info.startCountdown(crossingTime)
+        heldBerry = berrySprite
+        berrySprite.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+        berrySprite.setKind(SpriteKind.CollectedBerry)
+        berriesLeft -= 1 
+        //console.log("picked up berry")
+    }
+})
+function spawnBerries (numBerries: number, startColumn: number, startRow: number, gap: number) {
+    for (let index = 0; index < numBerries; index++) {
+        berry = sprites.create(img`
+            . . . . . . . 6 . . . . . . . . 
+            . . . . . . 8 6 6 . . . 6 8 . . 
+            . . . e e e 8 8 6 6 . 6 7 8 . . 
+            . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+            . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+            . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+            e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+            e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+            e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+            e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+            e 2 4 2 3 2 2 2 2 2 2 2 e c 6 . 
+            e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+            e e 2 e 2 2 4 2 2 e e e c . . . 
+            e e e e 2 e 2 2 e e e c . . . . 
+            e e e 2 e e c e c c c . . . . . 
+            . c c c c c c c . . . . . . . . 
+            `, SpriteKind.UncollectedBerry)
+        berry.z = -1
+        tiles.placeOnTile(berry, tiles.getTileLocation(startColumn, startRow))
+        startColumn += 1 + gap
+    }
+}
+let rightCar: Sprite = null
+let leftCar: Sprite = null
+let startColumn = 0
+let berry: Sprite = null
+let Fish: Sprite = null
+let heldBerry: Sprite = null
+scene.setBackgroundColor(7)
+tiles.setCurrentTilemap(tilemap`level2`)
+Fish = sprites.create(img`
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . c c 
+    . . . c 4 d 4 4 4 4 4 1 c c 4 c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 1 c f 4 4 1 f 4 f 
+    f 4 4 4 f 4 1 c 4 f 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f f f 
+    . f 4 4 4 4 1 c c 4 4 d f f . . 
+    . . f f 4 d 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(Fish, tiles.getTileLocation(5, 17))
+scene.cameraFollowSprite(Fish)
+Fish.setFlag(SpriteFlag.StayInScreen, true)
+spawnBerries(berriesLeft, 2, 1, 1)
+game.onUpdateInterval(500, function () {
+    leftCar = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 2 2 2 2 2 2 2 2 . . 
+        . . . . . 2 c 2 2 2 2 2 2 4 2 . 
+        . . . . 2 c c 2 2 2 2 2 2 4 c 2 
+        . . d 2 4 c c 2 4 4 4 4 4 4 c c 
+        . d 2 2 4 c b e e e e e e e 2 c 
+        . 2 2 2 4 b e e b b b e b b e 2 
+        . 2 2 2 2 2 e b b b b e b b b e 
+        . 2 2 2 2 e 2 2 2 2 2 e 2 2 2 e 
+        . 2 d d 2 e f e e e f e e e e e 
+        . d d 2 e e e f e e f e e e e e 
+        . e e e e e e e f f f e e e e e 
+        . e e e e f f f e e e e f f f f 
+        . . . e f f f f f e e f f f f f 
+        . . . . f f f f . . . . f f f . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(leftCar, sprites.vehicle.roadHorizontal)
+    leftCar.x = 180
+    leftCar.vx = -50
+    leftCar.setFlag(SpriteFlag.DestroyOnWall, true)
+})
+game.onUpdateInterval(500, function () {
+    rightCar = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 2 2 2 2 2 2 2 2 . . . . 
+        . . . 2 4 2 2 2 2 2 2 c 2 . . . 
+        . . 2 c 4 2 2 2 2 2 2 c c 2 . . 
+        . 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
+        . 2 c 2 e e e e e e e b c 4 2 2 
+        . 2 2 e b b e b b b e e b 4 2 2 
+        . 2 e b b b e b b b b e 2 2 2 2 
+        . e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
+        . e e e e e e f e e e f e 2 d d 
+        . e e e e e e f e e f e e e 2 d 
+        . e e e e e e f f f e e e e e e 
+        . e f f f f e e e e f f f e e e 
+        . . f f f f f e e f f f f f e . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(rightCar, assets.tile`myTile`)
+    rightCar.x = -20
+    rightCar.vx = 50
+    rightCar.setFlag(SpriteFlag.DestroyOnWall, true)
+})
+
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function(sprite : Sprite, otherSprite: Sprite){
+    info.changeLifeBy(-1)
+otherSprite.destroy()
+}
+
+
+
+)
